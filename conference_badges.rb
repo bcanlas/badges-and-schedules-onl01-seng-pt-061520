ae720ab40beffc486d24a326fbc146e748d3e726
Bianca Canlas <bcanlas87@gmail.com>
2020-06-30 04:13:16 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+
+def batch_badge_creator(array)
+  batch_badge_creator = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  badges. each do |atendees|
+       puts "Hello, my name is #{atendees}."
+  end
+batch_badge_creator
+end
